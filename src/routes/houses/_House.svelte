2765cfe4a090c41f26f01adde547623229f3bfd5
Paul Taylor <ptaylor@ricoh.com.au>
2019-10-30 08:57:06 +1100

    house list

---
+++ src/routes/houses/_House.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let picture;
+    export let type;
+    export let town;
+    export let title;
+    export let rating;
+    export let reviewsCount;
+    export let superhost;
+</script>
+
+<div>
+    <img src="{picture}" width="100%" style="" alt=""/>
+    <p>{type} - {town}</p>
+    <p>{title}</p>
+    <p>{rating} ({reviewsCount}) {superhost ? '- Superhost' : ''}</p>
+</div>
+
---
+++ src/routes/houses/_houses.js
@@ -0,0 +1,22 @@
+const houses = [
+    {
+        picture: "/img/houses/ostuni.png",
+        type: "Entire house",
+        town: "Ostuni",
+        title: "Beautiful flat in Ostuni!",
+        rating: 4.93,
+        reviewsCount: 198,
+        superhost: true
+    },
+    {
+        picture: "/img/houses/seashell.png",
+        type: "Entire house",
+        town: "Isla Mujeres",
+        title: "The World Famous Seashell House ~ Casa Caracol",
+        rating: 4.77,
+        reviewsCount: 246,
+        superhost: false
+    }
+]
+
+export default houses
